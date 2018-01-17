d6b1adf6937b2664c8692d329085bda82cb20dc1
jimjim <jimjim.lai@gmail.com>
2018-01-17 17:04:03 +0800

    L10L4套用bootstrap
    表格套用bootstrap完成

--- Gemfile
+++ Gemfile
@@ -48,3 +48,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#安裝掛載相片上傳器
+gem 'carrierwave'
+
+#安裝Bootstrap編輯器，並告訴他要安裝的版本
+gem 'bootstrap-sass', '~> 3.3.7'
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ApplicationRecord
-  /mount_up;oader :file_location, PhotoImageUpLoader/
+  mount_uploader :file_location, PhotoImageUploader # 在file_location掛載上傳器Photo_image_uploader.rb
   validates_presence_of :title, :date, :description, :file_location
 end
