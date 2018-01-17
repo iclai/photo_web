56be28a782475e48401cdf3c76c4dcdf7efd6cb6
jimjim <jimjim.lai@gmail.com>
2018-01-17 15:24:36 +0800

    S10L2 相簿可更新
    相簿可以上傳更新和編輯

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, :only => [:show, :edit, :update, :destroy] #用回呼應的方法 告訴Rails執行任何動作前先執行set_photo
+
   def index
     @photos = Photo.all
   end
@@ -9,40 +11,43 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photo_params)
-    @photo.save
+     if @photo.save
+      redirect_to photos_url #這是一個「重新導向」(Redirect) 的過程
+    else
+      render :action => :new #render 會保留使用者原本輸入的資料
+    end
+  end
 
+  def show    
+  end
+
+  def edit    
+  end
+
+  def update    
+    if @photo.update_attributes(photo_params)
+       redirect_to photo_path(@photo)
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy        
+    @photo.destroy    
     redirect_to photos_url
   end
 
-  def show
-    @photo = Photo.find(params[:id])
+#自行定義私有方法
+  private  
+
+  def set_photo
+    @photo = Photo.find(params[:id]) #因為Photo.find重複很多次，所以讓她簡化，定義一次重覆使用，讓程式碼更為簡潔。
+
   end
-
-  def edit
-    @photo = Photo.find(params[:id])
-  end
-
-  def update
-    @photo = Photo.find(params[:id])
-    @photo.update_attributes(photo_params)
-
-    redirect_to photo_path(@photo)
-  end
-
-  def destroy
-    @photo = Photo.find(params[:id])    
-    @photo.destroy
-    
-    redirect_to photos_url
-  end
-
-
-  private
 
   def photo_params
     params.require(:photo).permit(:title, :date, :description, :file_location)
 
   end
-
 
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,4 @@
 class Photo < ApplicationRecord
+  /mount_up;oader :file_location, PhotoImageUpLoader/
+  validates_presence_of :title, :date, :description, :file_location
 end
